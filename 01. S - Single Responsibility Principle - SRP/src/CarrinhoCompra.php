9c3c3350f67dca6d412b2bcbdb6730d1517870cb
dcunhamaciel <dcunhamaciel@gmail.com>
2023-05-02 20:27:34 -0300

    Criando Atributos e Métodos

--- 01. S - Single Responsibility Principle - SRP/src/CarrinhoCompra.php
+++ 01. S - Single Responsibility Principle - SRP/src/CarrinhoCompra.php
@@ -4,8 +4,59 @@ namespace App;
 
 class CarrinhoCompra
 {
-    public static function teste()
+    private $itens;
+    private $status;
+    private $valorTotal;
+
+    public function __construct()
     {
-        return 'ok';
+        $this->itens = [];
+        $this->status = 'aberto';
+        $this->valorTotal = 0;
+    }
+
+    public function exibirItens()
+    {
+        return $this->itens;
+    }
+
+    public function adicionarItem(string $item, float $valor)
+    {
+        array_push($this->itens, ["item" => $item, "valor" => $valor]);
+        $this->valorTotal += $valor;
+
+        return true;
+    }
+
+    public function exibirValorTotal()
+    {
+        return $this->valorTotal;
+    }
+
+    public function exibirStatus()
+    {
+        return $this->status;
+    }
+
+    public function confirmarPedido()
+    {
+        if (!$this->validarCarrinho()) {
+            return false;
+        }
+
+        $this->status = 'confirmado';
+        $this->enviarEmailConfirmacao();
+
+        return true;        
+    }
+
+    public function enviarEmailConfirmacao()
+    {
+        echo '<br>enviar e-mail de confirmação ... <br>';
+    }
+
+    public function validarCarrinho()
+    {
+        return count($this->itens) > 0;
     }
 }
