05f8f3ef2b079c3a35f8850a72fcecc99b5af021
dcunhamaciel <dcunhamaciel@gmail.com>
2023-05-08 20:13:26 -0300

    Criação do Projeto

---
+++ 02. O - Open Closed Principle - OCP/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'diego/app_etl',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '0408111c9ef1d68221636bb82d225a60f75ed345',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'diego/app_etl' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '0408111c9ef1d68221636bb82d225a60f75ed345',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
