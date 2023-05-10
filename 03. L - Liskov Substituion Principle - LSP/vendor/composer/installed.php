b48422f36a68162f094edab42673ff05ddf0d9db
dcunhamaciel <dcunhamaciel@gmail.com>
2023-05-10 19:22:18 -0300

    Criação do Projeto

---
+++ 03. L - Liskov Substituion Principle - LSP/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'diego/app_poligonos',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '65a84d3ceef020337fd3130f55c68f472699ac70',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'diego/app_poligonos' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '65a84d3ceef020337fd3130f55c68f472699ac70',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
