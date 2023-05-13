396c4ed15313f379078458f2d459fdaada26713b
dcunhamaciel <dcunhamaciel@gmail.com>
2023-05-13 11:41:03 -0300

    Criação do Projeto

---
+++ 04. I - Interface Segregation Principle - ISP/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'diego/app_crm',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'b675a0202793b070997d66921cefe7534d5269e2',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'diego/app_crm' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'b675a0202793b070997d66921cefe7534d5269e2',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
