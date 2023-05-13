635f38cff54da985cd29b07c9699777ea989c085
dcunhamaciel <dcunhamaciel@gmail.com>
2023-05-13 13:04:24 -0300

    Criação do Projeto

---
+++ 05. D - Dependency Inversion Principle - DIP/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'diego/app_mensageiro',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'dd0c2a7c6db29a36e59c2e44b02e93722885815f',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'diego/app_mensageiro' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'dd0c2a7c6db29a36e59c2e44b02e93722885815f',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
